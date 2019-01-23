8397f9b595a4b1edd5553199588faeee4c289af5
eras0r <er4s0r@gmail.com>
2019-01-23 20:39:30 +0100

    fix unit test for ItemsService

--- src/app/items/services/items.service.spec.ts
+++ src/app/items/services/items.service.spec.ts
@@ -1,9 +1,14 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ItemsService } from './items.service';
+import {ItemsService} from './items.service';
 
 describe('ItemsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    // TODO register provided service here hence 'providedIn' does not seem work in conjunction with ngrx effects (circular dependency)
+    providers: [
+      ItemsService
+    ]
+  }));
 
   it('should be created', () => {
     const service: ItemsService = TestBed.get(ItemsService);
