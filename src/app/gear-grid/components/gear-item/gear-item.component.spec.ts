75c5139b5578a6fb28e95aafacb7139697a43859
eras0r <er4s0r@gmail.com>
2019-01-23 21:15:44 +0100

    fix unit test for GearItemComponent

--- src/app/gear-grid/components/gear-item/gear-item.component.spec.ts
+++ src/app/gear-grid/components/gear-item/gear-item.component.spec.ts
@@ -1,25 +1,43 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { GearItemComponent } from './gear-item.component';
+import {GearItemComponent} from './gear-item.component';
+import {Item} from '../../../gear-grid-logic/item';
+import {By} from '@angular/platform-browser';
 
 describe('GearItemComponent', () => {
   let component: GearItemComponent;
   let fixture: ComponentFixture<GearItemComponent>;
+  const mockItem = new Item(1, 'test item');
+
+  const getId = () => fixture.debugElement.query(By.css('.col-sm-4'));
+
+  const getName = () => fixture.debugElement.query(By.css('.col-sm-8'));
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GearItemComponent ]
+      declarations: [GearItemComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GearItemComponent);
     component = fixture.componentInstance;
+    component.item = mockItem;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show the item\'s id', () => {
+    expect(getId().nativeElement.textContent).toContain(mockItem.id);
+  });
+
+  it('should show the item\'s name', () => {
+    expect(getName().nativeElement.textContent).toContain(mockItem.name);
+  });
+
 });
