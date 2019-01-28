2bd901584f168ab9e1030d1470620e473cc27178
erass0r <er4s0r@gmail.com>
2019-01-28 08:41:37 +0100

    include MaterialModule in TestingModule in order to provide access to Material components in unit tests

--- src/testing/utils.ts
+++ src/testing/utils.ts
@@ -3,6 +3,7 @@ import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {ActionsSubject, ReducerManager, StateObservable, Store, StoreModule} from '@ngrx/store';
 import {BehaviorSubject} from 'rxjs';
 import {RouterTestingModule} from '@angular/router/testing';
+import {MaterialModule} from '../app/material/material.module';
 
 @Injectable()
 export class MockStore<T> extends Store<T> {
@@ -33,12 +34,14 @@ export function provideMockStore() {
   imports: [
     NoopAnimationsModule,
     RouterTestingModule,
+    MaterialModule,
     // SharedModule, // TODO shared module as soon as it's available
     StoreModule.forRoot({})
   ],
   exports: [
     NoopAnimationsModule,
     RouterTestingModule,
+    MaterialModule
     // SharedModule, // TODO shared module as soon as it's available
   ],
   providers: [provideMockStore()]
