13c0e997d09cfe15784f54e760957fdd5e72dd38
erass0r <er4s0r@gmail.com>
2019-02-14 17:34:32 +0100

    initial version of gear grid component

--- src/app/gear-grid/components/gear-item/gear-item.component.ts
+++ src/app/gear-grid/components/gear-item/gear-item.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {Item} from '../../../gear-grid-logic/item';
+import {AffinityColor} from '../../../gear-grid-logic/affinity-color';
 
 @Component({
   selector: 'kdm-gear-item',
@@ -11,10 +12,40 @@ export class GearItemComponent implements OnInit {
 
   @Input() item: Item;
 
+  affinityColors = AffinityColor;
+
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  getAffinityColorClass(position: 'top' | 'left' | 'right' | 'bottom'): string {
+    switch (position) {
+      case 'top':
+        return this.getAffinityColorClassInner(this.item.topAffinity);
+      case 'left':
+        return this.getAffinityColorClassInner(this.item.leftAffinity);
+      case 'right':
+        return this.getAffinityColorClassInner(this.item.rightAffinity);
+      case 'bottom':
+        return this.getAffinityColorClassInner(this.item.bottomAffinity);
+      default:
+        return '';
+    }
+  }
+
+  private getAffinityColorClassInner(affinityColor: AffinityColor): string {
+    switch (affinityColor) {
+      case AffinityColor.Blue:
+        return 'affinity-color-blue';
+      case AffinityColor.Green:
+        return 'affinity-color-green';
+      case AffinityColor.Red:
+        return 'affinity-color-red';
+      default:
+        return '';
+    }
   }
 
 }
--- src/app/items/services/items.service.ts
+++ src/app/items/services/items.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {Item} from '../../gear-grid-logic/item';
+import {AffinityColor} from '../../gear-grid-logic/affinity-color';
 
 @Injectable()
 export class ItemsService {
@@ -11,7 +12,7 @@ export class ItemsService {
   loadItems(): Observable<Item[]> {
     // TODO replace mock items with real items obtained from server through HttpCLient
     return of([
-      new Item(1, 'test item 1'),
+      new Item(1, 'test item 1', AffinityColor.Red, AffinityColor.Green, AffinityColor.Red, AffinityColor.Blue),
       new Item(2, 'test item 2')
     ]);
   }
