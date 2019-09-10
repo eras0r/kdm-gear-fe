568da440e740b31e4516bd5ec3775af7df749c7f
eras0r <er4s0r@gmail.com>
2019-09-10 16:24:11 +0200

    ng update @angular/cdk @angular/material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatButtonModule, MatCardModule, MatGridListModule, MatIconModule, MatListModule, MatSidenavModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [],
